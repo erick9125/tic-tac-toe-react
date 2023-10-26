b1b122071b89c6695c09762e41148e93b41dc68a
Erick Morales <erick.morales@doctum.net>
2023-10-26 13:53:52 -0300

    completed sounds play and completd project

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -3,6 +3,13 @@ import Board from "./Board";
 import GameOver from "./GameOver";
 import GameState from "./GameState";
 import Reset from "./Reset";
+import gameOverSoundAsset from "../sounds/game_over.wav";
+import clickSoundAsset from "../sounds/click.wav";
+
+const gameOverSound = new Audio(gameOverSoundAsset);
+gameOverSound.volume = 0.2;
+const clickSound = new Audio(clickSoundAsset);
+clickSound.volume = 0.5;
 
 const PLAYER_X = "X";
 const PLAYER_O = "O";
@@ -65,6 +72,18 @@ function TicTacToe()
     useEffect(() => {
         checkWinner(tiles, setStrikeClass, setGameState);
     });
+
+    useEffect(() => {
+        if (tiles.some((tile) => tile !== null)) {
+          clickSound.play();
+        }
+      }, [tiles]);
+    
+      useEffect(() => {
+        if (gameState !== GameState.inProgress) {
+          gameOverSound.play();
+        }
+      }, [gameState]);
 
     const handleTileClick = (index) => {
 
